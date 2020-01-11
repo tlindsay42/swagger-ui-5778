aedb430fbb33b6dd69754a4143112758fd627bfb
Troy Lindsay <tlindsay42@users.noreply.github.com>
2020-01-10 18:27:37 -0600

    Add example code
    https://github.com/swagger-api/swagger-ui/issues/5778

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import SwaggerUI from 'swagger-ui';
+import 'swagger-ui/dist/swagger-ui.css';
 import './App.css';
+import swagger from './swagger.json';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  private elementID: string = 'swagger-ui';
+
+  public componentDidMount(): void {
+    this.swagger();
+  }
+
+  public componentDidUpdate(): void {
+    this.swagger();
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className='App'>
+        <div id={this.elementID} />
+      </div>
+    );
+  }
+
+  private swagger = (): void => {
+    SwaggerUI({
+      docExpansion: 'list',
+      domNode: document.getElementById(this.elementID),
+      spec: swagger,
+    });
+  }
 }
 
 export default App;
